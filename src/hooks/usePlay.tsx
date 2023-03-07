c9510962c48378d39865373c5f19c73f237409f3
Yves G <contact@webnumerik.fr>
2023-03-07 18:39:06 +0100

    feat:

--- src/hooks/usePlay.tsx
+++ src/hooks/usePlay.tsx
@@ -3,14 +3,14 @@ import { timer } from "../utils/timer";
 import { enlight, unenlight } from "../utils/ux-ui";
 
 function usePlay() {
-  const initialAwaleArray = [4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4];
+  const initialAwaleArray = [3, 2, 2, 2, 2, 2, 3, 2, 2, 2, 2, 2];
   const [awaleArray, setAwaleArray] = useState(initialAwaleArray);
   const [isPlaying, setIsPlaying] = useState(false);
   const [player, setPlayer] = useState(1);
   const [score, setScore] = useState([0, 0]);
 
   const play = async (index: number) => {
-    //je bloque la partie
+    //je bloque la partie pendant le déroulement de la fonction
     setIsPlaying(true);
     //si la partie est en cours, je ne fais rien
     if (isPlaying) return;
@@ -35,25 +35,39 @@ function usePlay() {
       if (index > 5) {
         index !== 11 ? (index += 1) : (index = 5);
         awaleArray[index] += 1;
+
         setAwaleArray([...awaleArray]);
         console.log("active: " + index);
         await unenlight();
         await enlight(index);
       }
     }
+    // ajouter au score du joueur si 2 ou 3 graines dans la case puis reset à 0
+    scoring(index);
     //je débloque la partie
     setIsPlaying(false);
-    scoreCalculaion();
+    // scoreCalculation(seedsNumber, index);
     await timer(300);
     await unenlight();
     whoIsPlaying();
+  };
+
+  const scoring = (index) => {
+    if (awaleArray[index] === 2 || awaleArray[index] === 3) {
+      index <= 5
+        ? setScore([score[0] + awaleArray[index], score[1]])
+        : setScore([score[0], score[1] + awaleArray[index]]);
+      awaleArray[index] = 0;
+      setAwaleArray([...awaleArray]);
+      return scoring(index > 5 ? index - 1 : index + 1);
+    }
   };
 
   const resetGame = async () => {
     setAwaleArray(initialAwaleArray);
     setPlayer(1);
     await unenlight();
-    scoreCalculaion();
+    setScore([0, 0]);
   };
 
   const whoIsPlaying = () => {
@@ -64,15 +78,31 @@ function usePlay() {
     }
   };
 
-  const scoreCalculaion = () => {
-    const scorePlayer1 = awaleArray.slice(6, 12).reduce((a, b) => {
-      if (b === 2 || b === 3) a + b;
-      console.log(a, b);
-      return a;
-    }, 0);
-    const scorePlayer2 = awaleArray.slice(0, 6).reduce((a, b) => a + b, 0);
-    setScore([scorePlayer1, scorePlayer2]);
+  const scoreCalculation = (seedsNumber, index) => {
+    for (let i = index; i > seedsNumber + 1; i--) {
+      console.log(seedsNumber, player, index, i);
+      if (player === 1) {
+        // const scorePlayer1 = awaleArray.slice(6, 12).reduce((a, b) => a + b, 0);
+        console.log("scorePlayer1");
+      }
+      if (player === 2) {
+        // const scorePlayer2 = awaleArray.slice(0, 6).reduce((a, b) => a + b, 0);
+        console.log("scorePlayer2");
+      }
+    }
   };
+
+  // const scoreCalculaion = () => {
+  //   const scorePlayer1 = awaleArray.slice(6, 12).reduce((a, b) => {
+  //     if (a === 2 || (a === 3 && b === 2) || b === 3) {
+  //       a + b;
+  //       console.log(a, b, a + b);
+  //     }
+  //     return a + b;
+  //   }, 0);
+  //   const scorePlayer2 = awaleArray.slice(0, 6).reduce((a, b) => a + b, 0);
+  //   setScore([scorePlayer1, scorePlayer2]);
+  // };
 
   const checkIfGameIsOver = () => {};
 
--- src/views/AwaleContainer.tsx
+++ src/views/AwaleContainer.tsx
@@ -4,10 +4,12 @@ import usePlay from "../hooks/usePlay";
 function AwaleContainer() {
   const [awaleArray, play, resetGame, player, score] = usePlay();
   return (
-    <>
-      <h1>
-        Score: <br /> <b>Player 1</b> {score[0]} - <b>Player 2</b> {score[1]}
-      </h1>
+    <main className="flex flex-col">
+      <h1 className="text-9xl">AWALE</h1>
+      Score:
+      <p>
+        <b>Player 1</b> {score[0]} - <b>Player 2</b> {score[1]}
+      </p>
       {/* {score[0] > score[1] && <h1>Player 1 wins</h1>} */}
       Player {player} turn
       <ul className="list-none bg-[#421b00] w-[800px] rounded-[1rem] p-8 flex flex-wrap">
@@ -22,16 +24,27 @@ function AwaleContainer() {
           );
         })}
       </ul>
-      {
-        <button
-          className="bg-[#000000] text-[#f2e9e4] text-[1.5rem] font-bold rounded-[1rem] p-4 m-4"
-          type="reset"
-          onClick={() => resetGame()}
+      <footer className=" items-center flex justify-evenly">
+        {
+          <button
+            className="bg-[#000000] text-[#f2e9e4] text-[1.5rem] font-bold rounded-[1rem] p-4 m-4 max-w-[10rem]"
+            type="reset"
+            onClick={() => resetGame()}
+          >
+            RESET
+          </button>
+        }
+        <a
+          target="_blank"
+          href="https://www.youtube.com/watch?v=atg9dZq2q0c&ab_channel=Videoregles.Net"
         >
-          RESET
-        </button>
-      }
-    </>
+          Les règles sur YouTube
+        </a>
+        <a target="_blank" href="https://fr.wikipedia.org/wiki/Awal%C3%A9">
+          Les règles sur WikiPedia
+        </a>
+      </footer>
+    </main>
   );
 }
 
