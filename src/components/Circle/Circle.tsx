4dfcb4c7ed572fe9c7310d7073a4443d3804d3d7
Yves G <contact@webnumerik.fr>
2023-03-16 16:08:28 +0100

    style:

--- src/components/Circle/Circle.tsx
+++ src/components/Circle/Circle.tsx
@@ -8,7 +8,7 @@ type CircleProps = {
 function Circle({ seedNumber, clickHandler, index, player }: CircleProps) {
   return (
     <li
-      className={`lg:w-[100px] lg:h-[100px] w-[82px] h-[82px] border-solid border-2 border-indigo-60 rounded-full m-[.7rem]  hover:bg-[#a3461b] cursor-pointer flex justify-center items-center transition duration-150 ease-out hover:ease-in hover:scale-[1.2] 
+      className={`lg:w-[100px] lg:h-[100px] w-[82px] h-[82px] border-solid border-2 border-indigo-60 rounded-full m-[auto]  hover:bg-[#a3461b] cursor-pointer flex justify-center items-center transition duration-150 ease-out hover:ease-in hover:scale-[1.2] 
       ${
         (player === 1 && [0, 1, 2, 3, 4, 5].includes(index)) ||
         (player === 2 && [6, 7, 8, 9, 10, 11].includes(index))
