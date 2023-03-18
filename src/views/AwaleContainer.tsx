b215950a136a7e8ee77cfaaff687bc4b43c8a0be
Yves G <contact@webnumerik.fr>
2023-03-18 21:11:31 +0100

    style:

--- src/views/AwaleContainer.tsx
+++ src/views/AwaleContainer.tsx
@@ -7,9 +7,9 @@ function AwaleContainer() {
   const gameCore = useContext(GameContext);
   return (
     <main className="relative flex lg:flex-col items-center min-w-[360px]">
-      <PlayerSide player={2} />
-      <WhoseTurn player={gameCore.player} />
       <PlayerSide player={1} />
+      <WhoseTurn player={gameCore.player} />
+      <PlayerSide player={2} />
     </main>
   );
 }
--- src/views/Top.tsx
+++ src/views/Top.tsx
@@ -9,11 +9,11 @@ function Top() {
   return (
     <header className="lg:mb-8 static">
       <section className="flex justify-evenly items-center">
-        <Score score={gameCore.score} player={2} />
+        <Score score={gameCore.score} player={1} />
         <h1 className={`lg:text-8xl text-2xl text-brown`}>
           AWALE
         </h1>
-        <Score score={gameCore.score} player={1} />
+        <Score score={gameCore.score} player={2} />
       </section>
     </header>
   );
